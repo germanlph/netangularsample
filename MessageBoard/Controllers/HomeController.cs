c3b8327dbbf19126d2569bdcc8e29c83071dd19c
German Portillo <germanlph@gmail.com>
2017-03-18 12:30:01 -0500

    Initial Commmit

---
+++ MessageBoard/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using MessageBoard.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = String.Format("Comment from: {1}{0}Email: {2}{0}Website: {3}{0}Comment: {4}{0}",
+                Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+
+            if (_mail.SendMail("noreply@yourdomain.com", "foo@yourdomain.com", "Website Contact", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+    }
+   
+}
